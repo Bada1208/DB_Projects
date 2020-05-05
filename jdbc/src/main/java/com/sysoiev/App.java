3e6ba7b1619b2e3992db04994d365fbb0934b518
Bada1208 <sysoievbogdan@gmail.com>
2020-05-05 10:55:26 +0200

    added executeUpdate method

--- jdbc/src/main/java/com/sysoiev/App.java
+++ jdbc/src/main/java/com/sysoiev/App.java
@@ -5,14 +5,19 @@ import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class App {
     public static void main(String[] args) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            String createTable = "CREATE TABLE IF NOT EXISTS products (Id INT PRIMARY KEY AUTO_INCREMENT, ProductName VARCHAR(20), Price INT)";
             try (Connection connection = getConnection()) {
-                System.out.println("Connection successful");
+                Statement statement = connection.createStatement();
+                // создание таблицы
+                statement.executeUpdate(createTable);
+                System.out.println("Table has been created");
             }
         } catch (Exception e) {
             System.out.println("Connection failed...");
@@ -22,6 +27,7 @@ public class App {
     }
 
     public static Connection getConnection() throws IOException, SQLException {
+
         Properties properties = new Properties();
         try (InputStream inputStream = App.class.getResourceAsStream("/database.properties")) {
             properties.load(inputStream);
