69ebf0ea2bc3e1081580b11dbbec553c4c6360cf
Bada1208 <sysoievbogdan@gmail.com>
2020-05-05 10:27:38 +0200

    added properties file & connection in a apart method

--- jdbc/src/main/java/com/sysoiev/App.java
+++ jdbc/src/main/java/com/sysoiev/App.java
@@ -1,22 +1,34 @@
 package com.sysoiev;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class App {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/store_jdbc?serverTimezone=UTC";
-        String username = "root";
-        String password = "root";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
-            try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            try (Connection connection = getConnection()) {
                 System.out.println("Connection successful");
             }
         } catch (Exception e) {
-            System.out.println("Connection failed");
-            System.out.println(e);
+            System.out.println("Connection failed...");
 
+            System.out.println(e);
         }
+    }
+
+    public static Connection getConnection() throws IOException, SQLException {
+        Properties properties = new Properties();
+        try (InputStream inputStream = App.class.getResourceAsStream("/database.properties")) {
+            properties.load(inputStream);
+        }
+        String url = properties.getProperty("url");
+        String username = properties.getProperty("username");
+        String password = properties.getProperty("password");
+        return DriverManager.getConnection(url, username, password);
     }
 }
