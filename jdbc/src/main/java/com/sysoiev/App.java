5f06f647d029346b88df7dc31322962ad7b4e73a
Bada1208 <sysoievbogdan@gmail.com>
2020-05-05 14:41:04 +0200

    added update, drop, delete

--- jdbc/src/main/java/com/sysoiev/App.java
+++ jdbc/src/main/java/com/sysoiev/App.java
@@ -9,7 +9,16 @@ import java.sql.Statement;
 import java.util.Properties;
 
 public class App {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException, SQLException {
+        dropTable();
+        tableCreation();
+        insertData();
+        updateData();
+        deleteData();
+    }
+
+    public static void tableCreation() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
             String createTable = "CREATE TABLE IF NOT EXISTS products (Id INT PRIMARY KEY AUTO_INCREMENT, ProductName VARCHAR(20), Price INT)";
@@ -18,9 +27,6 @@ public class App {
                 // создание таблицы
                 statement.executeUpdate(createTable);
                 System.out.println("Table has been created");
-                int rows = statement.executeUpdate("INSERT products(ProductName, Price) VALUES ('iPhone X', 76000)," +
-                        "('Galaxy S9', 45000), ('Nokia 9', 36000)");
-                System.out.printf("Added %d rows", rows);
             }
         } catch (Exception e) {
             System.out.println("Connection failed...");
@@ -39,5 +45,37 @@ public class App {
         String username = properties.getProperty("username");
         String password = properties.getProperty("password");
         return DriverManager.getConnection(url, username, password);
+    }
+
+    public static void insertData() throws SQLException, IOException {
+        try (Connection connection = getConnection()) {
+            Statement statement = connection.createStatement();
+            int rows = statement.executeUpdate("INSERT products(ProductName, Price) VALUES ('iPhone X', 76000)," +
+                    "('Galaxy S9', 45000), ('Nokia 9', 36000)");
+            System.out.printf("Added %d rows", rows);
+        }
+    }
+
+    public static void dropTable() throws IOException, SQLException {
+        try (Connection connection = getConnection()) {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("DROP TABLE products");
+        }
+    }
+
+    public static void updateData() throws IOException, SQLException {
+        try (Connection connection = getConnection()) {
+            Statement statement = connection.createStatement();
+            int rows = statement.executeUpdate("UPDATE products SET Price = Price - 5000");
+            System.out.printf("\nUpdated %d rows", rows);
+        }
+    }
+
+    public static void deleteData() throws IOException, SQLException {
+        try (Connection connection = getConnection()) {
+            Statement statement = connection.createStatement();
+            int rows = statement.executeUpdate("DELETE FROM products WHERE Id = 3");
+            System.out.printf("\n%d row(s) deleted", rows);
+        }
     }
 }
