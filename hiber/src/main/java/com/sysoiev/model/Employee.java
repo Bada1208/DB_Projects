273fae29a1a09390a2d13d8a2552c4e78672246e
Bada1208 <sysoievbogdan@gmail.com>
2020-05-16 22:29:31 +0200

    was written ordinary hiber project

--- hiber/src/main/java/com/sysoiev/model/Employee.java
+++ hiber/src/main/java/com/sysoiev/model/Employee.java
@@ -2,7 +2,17 @@ package com.sysoiev.model;
 
 public class Employee {
     private int id;
-    private String firstName, lastName;
+    private String firstName;
+    private String lastName;
+
+    public Employee() {
+    }
+
+    public Employee(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
     public int getId() {
         return id;
